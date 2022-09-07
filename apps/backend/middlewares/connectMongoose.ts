02650fb5be214c5b136b1957e9464558c0d28862
DrowningToast <supratouchgussii@gmail.com>
2022-09-07 12:25:46 +0700

    Wallet

--- apps/backend/middlewares/connectMongoose.ts
+++ apps/backend/middlewares/connectMongoose.ts
@@ -9,14 +9,11 @@ const useMongoose = async (req, res, next) => {
     return next();
   }
   try {
-    console.log("Connecting to the database");
     await mongoose.connect(
       `mongodb+srv://${process.env.db_username}:${process.env.db_password}@itggmongodb.ppba9ee.mongodb.net/?retryWrites=true&w=majority`
     );
-    console.log("Database connected");
     next();
   } catch (e) {
-    console.log("The database is failed to connect");
     console.log(e);
   }
 };
@@ -24,9 +21,7 @@ const useMongoose = async (req, res, next) => {
 let connection;
 
 const connectDB = async (App: Express) => {
-  console.log("conecting");
   try {
-    console.log(mongoose.connection[0]);
     connection = await mongoose.connect(
       `mongodb+srv://${process.env.db_username}:${process.env.db_password}@itggmongodb.ppba9ee.mongodb.net/?retryWrites=true&w=majority`
     );
